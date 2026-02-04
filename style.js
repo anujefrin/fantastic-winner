0224a6c931859f69ba7ce5e641aaa7cc66c6b9dc
anujefrin <anujefrin14@gmail.com>
2026-02-04 20:42:10 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,9 @@
+function goToPage2() {
+    document.getElementById("page1").classList.remove("active");
+    document.getElementById("page2").classList.add("active");
+}
+
+function goToPage3() {
+    document.getElementById("page2").classList.remove("active");
+    document.getElementById("page3").classList.add("active");
+}
